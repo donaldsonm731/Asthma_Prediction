acbce4210d04c91850876c546613f00ad3153c72
kadieiverson <i.kadie@wustl.edu>
2022-03-30 17:24:05 -0500

    Update app.R

--- ESE527/app.R
+++ ESE527/app.R
@@ -1,49 +1,24 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+## Only run examples in interactive R sessions
+if (interactive()) {
+  shinyApp(
+    ui = fluidPage(
+      selectInput("age", "What is your age?:",
+                  list("19-30", "31-40", "41-50", "Above 50")
+      ),
+      selectInput("gender", "What is your gender?:",
+                  list("Female", "Male")
+      ),
+      textOutput("result")
+    ),
+    server <- function(input, output, session) {
+      
+      data <- reactive(asthmaData)
+      data.label <- reactive(jstable::mk.lev(asthmaData))
+      
+      out_linear <- callModule(fm1, "linear", data = data, data_label = data.label,
+                               data_varStruct = NULL)
+      
+      output$lineartable <- renderDT({
+        datatable(out_linear()$table, rownames=T, caption = out_linear()$caption)
+      })}
+  )}
